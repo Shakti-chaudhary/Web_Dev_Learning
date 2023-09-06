9a91bc1f2e112a74a881385dc6a3f0936dbfc874
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-09-07 00:27:34 +0530

    Day 6

--- HTML CSS JAVASCRIPT/01_project_ToDo/app.js
+++ HTML CSS JAVASCRIPT/01_project_ToDo/app.js
@@ -2,6 +2,44 @@ console.log("hello world!");
 
 // Selectors
 
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
 // Event Listeners
 
+todoButton.addEventListener("click", addTodo);
+
 // Functions
+
+function addTodo(event) {
+  event.preventDefault();
+  console.log("clicked");
+  //   document.write("hello!");
+
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  // Check Mark Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // Check Trash Button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+  console.log(todoDiv);
+
+  // Append ToDo List
+  todoList.appendChild(todoDiv);
+}
