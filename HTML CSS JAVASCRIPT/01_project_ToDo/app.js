cd087b4be06f702aee9f9d9a8f83cf8beada970f
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-09-07 13:41:41 +0530

    Day 6

--- HTML CSS JAVASCRIPT/01_project_ToDo/app.js
+++ HTML CSS JAVASCRIPT/01_project_ToDo/app.js
@@ -9,13 +9,12 @@ const todoList = document.querySelector(".todo-list");
 // Event Listeners
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", buttonCheck);
 
 // Functions
 
 function addTodo(event) {
   event.preventDefault();
-  console.log("clicked");
-  //   document.write("hello!");
 
   // Todo DIV
   const todoDiv = document.createElement("div");
@@ -23,7 +22,7 @@ function addTodo(event) {
 
   // Create LI
   const newTodo = document.createElement("li");
-  newTodo.innerText = "hey";
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
@@ -36,10 +35,34 @@ function addTodo(event) {
   // Check Trash Button
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
-  deleteButton.classList.add("delete-btn");
+  deleteButton.classList.add("trash-btn");
   todoDiv.appendChild(deleteButton);
-  console.log(todoDiv);
 
   // Append ToDo List
   todoList.appendChild(todoDiv);
+
+  // Clear Todo Input value
+  todoInput.value = "";
+}
+
+function buttonCheck(event) {
+  // console.log(event.target);
+  const item = event.target;
+
+  // Delete Todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+
+    // Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  // Check Mark
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
