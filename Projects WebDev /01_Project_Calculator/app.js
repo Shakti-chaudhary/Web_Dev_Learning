32fca4af84802e70afe2f8d55d7f7b09368058c1
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-09-09 01:21:17 +0530

    Day 7

--- Projects WebDev /01_Project_Calculator/app.js
+++ Projects WebDev /01_Project_Calculator/app.js
@@ -1,1 +1,19 @@
-  
+// Selectors
+
+let items = document.querySelectorAll(".grid-items");
+
+// Event Listeners
+
+items.forEach((item) => {
+  item.addEventListener("click", changeColor);
+});
+
+// Functions
+
+function changeColor(event) {
+  let clickItem = event.target;
+  clickItem.classList.add("clicked");
+  setTimeout(() => {
+    clickItem.classList.remove("clicked");
+  }, 100);
+}
