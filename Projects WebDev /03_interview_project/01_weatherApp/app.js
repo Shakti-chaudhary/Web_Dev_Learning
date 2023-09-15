85598475564396e8544a0e0552ffe2bf27850744
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-09-15 23:38:08 +0530

    Day 9

--- Projects WebDev /03_interview_project/01_weatherApp/app.js
+++ Projects WebDev /03_interview_project/01_weatherApp/app.js
@@ -1,7 +1,21 @@
 // Variables
+const apiKey = "2ee9a323e1b35335a8091c873209eb2f";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=meerut";
+
+//   q=meerut&appid=
 
 // Selectors
 
 // Event Listeners
 
 // Functions
+
+async function checkWeather() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  console.log(response.status);
+
+  var data = await response.json();
+  console.log(data.weather[0].main);
+}
+checkWeather();
