5235f8946198f08ac8ca01b3a31e5c797a7f1893
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-09-20 01:39:34 +0530

    Day 10 Weather app complete

--- Projects WebDev /03_interview_project/01_weatherApp/app.js
+++ Projects WebDev /03_interview_project/01_weatherApp/app.js
@@ -3,8 +3,6 @@ const apiKey = "2ee9a323e1b35335a8091c873209eb2f";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-//   q=meerut&appid=
-
 // Selectors
 
 const searchBox = document.querySelector(".search input");
@@ -16,10 +14,7 @@ const temp = document.querySelector(".temp");
 const humidity = document.querySelector(".humidity");
 const wind = document.querySelector(".wind");
 const weatherIcon = document.querySelector(".weather-icon");
-
-// StartUp function
-
-// checkWeather("New York");
+const error = document.querySelector(".error");
 
 // Event Listeners
 
@@ -36,10 +31,23 @@ searchBtn.addEventListener("click", () => {
 
 async function checkWeather(searchCity) {
   const response = await fetch(apiUrl + searchCity + `&appid=${apiKey}`);
-  var data = await response.json();
-  city.innerHTML = data.name;
-  temp.innerHTML = Math.round(data.main.temp) + "°C";
-  humidity.innerHTML = data.main.humidity + "%";
-  wind.innerHTML = Math.round(data.wind.speed) + " km/h";
-  weather.style.display = "block";
+
+  if (response.status == 404) {
+    error.style.display = "block";
+    weather.style.display = "none";
+  } else {
+    var data = await response.json();
+    city.innerHTML = data.name;
+    temp.innerHTML = Math.round(data.main.temp) + "°C";
+    humidity.innerHTML = data.main.humidity + "%";
+    wind.innerHTML = Math.round(data.wind.speed) + " km/h";
+    error.style.display = "none";
+    weather.style.display = "block";
+
+    // Weather icon
+    let iconCode = data.weather[0].icon;
+
+    let iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+    weatherIcon.src = iconUrl;
+  }
 }
