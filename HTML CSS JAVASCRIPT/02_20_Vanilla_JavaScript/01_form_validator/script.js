db5a5f392ecf6c0e3f579f8fc4797137e8515092
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-10-20 21:47:23 +0530

    Day 15

--- HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/01_form_validator/script.js
+++ HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/01_form_validator/script.js
@@ -1,2 +1,29 @@
-// starting with js to check for running .
-console.log("Hello world!");
+// Variables
+
+// Selectors
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// Event Listeners
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+  console.log(username.value);
+});
+
+// Functions
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.classList.add("error");
+}
