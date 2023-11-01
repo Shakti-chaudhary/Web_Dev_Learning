19647a8f3a8d00ae49d563d39818b570cbfda844
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-11-02 01:29:02 +0530

    Day 16 form validation complete

--- HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/01_form_validator/script.js
+++ HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/01_form_validator/script.js
@@ -1,5 +1,3 @@
-// Variables
-
 // Selectors
 
 const form = document.getElementById("form");
@@ -13,46 +11,81 @@ const password2 = document.getElementById("password2");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  checkRequired([username, email, password, password2]);
+  // Run validations
+  validateRequiredFields([username, email, password, password2]);
+  validateLength(username, 3, 15);
+  validateLength(password, 6, 25);
+  validateEmail(email);
+  validatePasswordMatch(password, password2);
 });
 
 // Functions
 
-// Check required fields
-function checkRequired(inputArr) {
-  inputArr.forEach((input) => {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
+// Set form message
+function setFormMessage(formControl, type, message) {
+  formControl.classList.remove("error");
+  formControl.classList.add(type);
+  formControl.querySelector("small").innerText = message;
 }
 
-// Feild first name capital
+// Show error massage
+function showError(input, message) {
+  setFormMessage(input.parentElement, "error", message);
+}
+
+// Show success massage
+function showSuccess(input) {
+  setFormMessage(input.parentElement, "success", "");
+}
+
+// Get fiel name fist char cappital
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-// Show input error message
-function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.classList.add("error");
-  const small = formControl.querySelector("small");
-  small.innerText = message;
+// Validation required fields
+function validateRequiredFields(requiredFields) {
+  requiredFields.forEach((field) => {
+    if (field.value.trim() === "") {
+      showError(field, `${getFieldName(field)} is required`);
+    } else {
+      showSuccess(field);
+    }
+  });
 }
 
-//Show success outline
-function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.classList.remove("error");
-  formControl.classList.add("success");
+// Validate lengths of fields
+function validateLength(field, min, max) {
+  if (field.value.length < min) {
+    showError(
+      field,
+      `${getFieldName(field)} must be at least ${min} characters`
+    );
+  } else if (field.value.length > max) {
+    showError(
+      field,
+      `${getFieldName(field)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(field);
+  }
 }
 
-// Checks email is valid
-function isEmailValid(str) {
-  var pattern =
+// validatte email
+function validateEmail(email) {
+  const re =
     /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i;
 
-  return pattern.test(str.value);
+  if (re.test(email.value)) {
+    showSuccess(email);
+  } else {
+    showError(email, "Email is not valid");
+  }
+}
+
+// Validate password match
+function validatePasswordMatch(password, password2) {
+  if (password.value !== password2.value) {
+    showError(password2, "Passwords do not match");
+  }
 }
