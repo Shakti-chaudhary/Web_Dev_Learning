672e8246f519464edbaa39cc069ae5c5f54ac284
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-11-04 00:57:16 +0530

    Day 17 js add

--- HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/02_movie_seat/script.js
+++ HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/02_movie_seat/script.js
@@ -1,1 +1,51 @@
-console.log("Hello world! checking for connection");
+// Selectors
+
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+let ticketPrice = +movieSelect.value;
+
+// Event Listners
+
+// Movie select event
+movieSelect.addEventListener("change", (event) => {
+  ticketPrice = +event.target.value;
+  setMovieData(event.target.selectedIndex, event.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("seat") &&
+    !event.target.classList.contains("occupied")
+  ) {
+    event.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+// Functions
+
+// Save movie index to local storage
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
