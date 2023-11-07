eafcb53578c8b53e57fbf050ef348b0fec617185
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-11-08 01:56:22 +0530

    Day 19 js add module complete

--- HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/05_DOM_array/script.js
+++ HTML CSS JAVASCRIPT/02_20_Vanilla_JavaScript/05_DOM_array/script.js
@@ -8,7 +8,8 @@ let data = [];
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add-user");
 const doubleBtn = document.getElementById("double");
-const showMillionairesBtn = document.getElementById("show-milloinaires");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
@@ -16,19 +17,23 @@ const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
 
 // Functions
 
 // Fetch random user and add money
 async function getRandomUser() {
   const response = await fetch(userGenerateApi);
-  const data = await response.json();
-  const user = data.results[0];
+  const apiData = await response.json();
+  const user = apiData.results[0];
 
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
   };
+
   addData(newUser);
 }
 
@@ -56,7 +61,7 @@ function updateDOM(provideData = data) {
 
 // Formate number  as money
 function formateMoney(money) {
-  return +money.toLocaleString("en-US", {
+  return money.toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
@@ -68,4 +73,28 @@ function doubleMoney() {
     return { ...user, money: +user.money * 2 };
   });
   updateDOM(data);
+}
+
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("h3");
+  wealthEl.innerHTML = ` Total Wealth: <strong>${formateMoney(
+    wealth
+  )} </strong> `;
+  main.appendChild(wealthEl);
 }
