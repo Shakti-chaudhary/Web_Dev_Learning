9c1e9798178671418412ffe03039bf46faeff3fd
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-11-26 18:45:24 +0530

    Day 24 React start learn

---
+++ REACTJS/01_React_Learn/01_react_first/src/App.js
@@ -0,0 +1,22 @@
+import "./app.css";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, odio
+          deserunt. Facere doloribus, et in molestiae earum commodi aliquid
+          culpa aliquam odio hic nisi similique, voluptatem iste dolorum alias
+          corrupti assumenda numquam ullam quam.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
