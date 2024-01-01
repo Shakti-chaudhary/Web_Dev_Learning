25f5d9828a94e2e1c269b3ef769d33353a689732
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2024-01-02 00:02:33 +0530

    Day 25 js add

--- REACTJS/01_React_Learn/02_customReact/customreact.js
+++ REACTJS/01_React_Learn/02_customReact/customreact.js
@@ -1,17 +1,19 @@
-function customRender (){
-    const domElement = 
-
-};
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+}
 
 const reactElement = {
-    type: 'a'
-    ,props: {
-        href:"https://google.com"
-        ,target: "_blank"
-    }
-    ,
-    children: 'Click me to visit google'
-}
-const mainContainer = document.querySelector("#root")
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+const mainContainer = document.querySelector("#root");
 
-customRender(reactElement,mainContainer)
+customRender(reactElement, mainContainer);
